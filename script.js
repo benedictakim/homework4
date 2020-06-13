599790a18dfec2f860df7efe179ba7d944377511
Benn <61118590+benedictakim@users.noreply.github.com>
2020-06-13 14:22:59 -0400

    modified:   index.html -added button id modified:   script.js -pseudocode, start of first set coding

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 //pseudocode
 
-//click "Start Quiz" - timer counts down from 75 seconds or 7500 msec
+//click "Start Quiz" - timer counts down from 75 seconds or 75,0000 msec
+var quiztime = "75000"
+document.getElementById("start").onclick = function (){
+ 
+}
 
-//
+//Click "Start Quiz" - display question
+
+//Answer question, display right/wrong answer, display next question
+
+//if wrong answer, decrease timer by 10 seconds or 10,000 msec
+
+//if all questions answered or timer is 0, game over
+
+//scoring: if all questions answered and time is >0, then score is 75 - timeleft
+
+//game over, enter name and save name and score, then save score to local storage and then display high score page
+
+//high score page: display name and score with button to "go back"/"play again" and "clear high score"
 
 
 
